269515807d4b8fac2cabaf8925f8c51bcd3435a0
Evgeniy Koynov <koinoff@gmail.com>
2018-09-20 00:43:53 +0300

    audited sol update

--- Solidity/contracts/audited.sol
+++ Solidity/contracts/audited.sol
@@ -7,7 +7,6 @@ import "./owned.sol";
 // ----------------------------------------------------------------------------
 contract Audited is Owned{
     address public auditor;
-    mapping (uint64 => bool) public confirmed;
 
     event SetAuditor(address indexed _to);
     event SetAuditResult(uint64 indexed _time, bool _status);
@@ -23,12 +22,6 @@ contract Audited is Owned{
 
     function setAuditor(address _newAuditor) public onlyOwner {
         auditor = _newAuditor;
-    }
-
-    function setAuditResult(uint64 time, bool status) public onlyAuditor returns (bool success) {
-        confirmed[time] = status;
-        emit SetAuditResult(time, status);
-        return true;
     }
 
 }
